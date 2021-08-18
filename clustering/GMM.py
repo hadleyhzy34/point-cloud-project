021fdd279f7240fa9d587d577da94755014035f4
hadleyhzy34 <hadley_hzy@hotmail.com>
2021-08-18 21:13:14 +0800

    GMM update

--- clustering/GMM.py
+++ clustering/GMM.py
@@ -1,7 +1,14 @@
-# 文件功能：实现 GMM 算法
+# GMM algorithm
+
+# to do list: 
+# 1.matrix calculation for matrix inside fit function
+# 2.draw trajetory for mu update history
+# 3.make it stopped when convergence
+# 4.prevent cov too small or too large
 
 import numpy as np
 from numpy import *
+import numpy
 import pylab
 import random,math
 
@@ -34,52 +41,81 @@ class GMM(object):
     # 屏蔽结束
     
     def fit(self, data):
+        # import ipdb;ipdb.set_trace()
+        # initialize parameters
         data_min = np.min(data,axis=0)
         data_max = np.max(data,axis=0)
 
-        num = data.shape[0]
+        num = data.shape[0] # number of data
         #initialize prior distribution probability
-        self.prior_dist = np.ones((num,self.n_clusters))*(1/self.n_clusters)
+        self.prior_dist = np.ones(self.n_clusters)*(1/self.n_clusters) # pi, (n_clusters)
 
         #initialize n_cluster guassian distribution
-        mu = np.random.randn(self.k_,2)*(data_max-data_min)+data_min
-        var = 
-       
-        for _ in range(self.max_iter_):
-            # calculate prob for ith data and jth gaussian distribution
+        self.mu = np.random.randn(self.n_clusters,2)*(data_max-data_min)+data_min  # mu, (n_clusters, 2)
+        # self.var = np.ones((self.n_clusters,2,2)) * np.cov(data.T) # var, (n_clusters, 2, 2)
+        self.var = np.ones((self.n_clusters,2,2)) * (np.eye(2) * np.array([np.cov(data.T)[0][0],np.cov(data.T)[1][1]]))
+        # self.var = np.cov(data.T)  # var, (2,2)
 
-            # calculate pi
+        # initialize posterior probability
+        self.epsilon = np.zeros((num,self.n_clusters)) # (num, n_clusters)
 
-            # calculate mu
-
-            # calculate var
-
-            #
-
-            #
-            # recalculate centers
-            self.centers = np.zeros((self.k_,3))
-            for i in range(data.shape[0]):
-                index = np.argmin(distance[i,:])
-                self.centers[index] += np.array([data[i][0],data[i][1],1])
-            
-            for j in range(self.k_):
-                if self.centers[j][2] == 0:
-                    continue
-                self.centers[j,0:2] = self.centers[j,0:2]/self.centers[j][2]
-                # print(f'current centers are: {self.centers[j]}')
-    
-    def predict(self, data):
-        result = []
-        distance = (np.array([data[:,0],]*self.k_).transpose()-self.centers[:,0])**2 + \
-            (np.array([data[:,1],]*self.k_).transpose()-self.centers[:,1])**2
+        # initialize multivariate guassian probability density function
+        self.distribution = np.ones((num,self.n_clusters)) # (num, n_clusters)
         
+        for i in range(num):
+            for j in range(self.n_clusters):
+                self.distribution[i][j] = multivariate_normal.pdf(data[i], mean=self.mu[j],cov=self.var[j])
+       
+        # import ipdb;ipdb.set_trace()
+        for _ in range(self.max_iter):
+            # # calculate distance from points to gausian mu
+            # distance = (np.array([data[:,0],]*self.k_).transpose()-self.centers[:,0])**2 + \
+            #     (np.array([data[:,1],]*self.k_).transpose()-self.centers[:,1])**2
+            
+            # calculate posterior probability
+            self.epsilon = self.prior_dist*self.distribution / (np.array([np.mean(self.prior_dist*self.distribution,axis=1),]*self.n_clusters).transpose())
+            
+            # update mu
+            self.mu = np.zeros((self.n_clusters,2))
+            for j in range(self.n_clusters):
+                self.mu[j] = np.array(np.sum(data[:,0]*self.epsilon[:,j]),np.sum(data[:,1]*self.epsilon[:,j]))
+            # for j in range(self.n_clusters):
+            #     for i in range(num):
+            #         self.mu[j] += data[i] * self.epsilon[i,j]
+            #     # import ipdb;ipdb.set_trace()
+            #     self.mu[j] = self.mu[j] / np.sum(self.epsilon[:,j])
+
+            # import ipdb;ipdb.set_trace()
+            # calculate pi
+            self.prior_dist = np.sum(self.epsilon,axis=0)/self.n_clusters
+
+            # import ipdb;ipdb.set_trace()
+            for j in range(self.n_clusters):
+                for i in range(num):
+                    self.var[j] += self.epsilon[i,j]*(np.eye(2)*np.diag(np.dot(np.array([data[i]-self.mu[j],]).transpose(),np.array([data[i]-self.mu[j],]))))
+                self.var[j] = self.var[j] / np.sum(self.epsilon[:,j])
+
+            # update distribution
+            for i in range(num):
+                for j in range(self.n_clusters):
+                    # print(f'current data: {data[i]}, mean: {self.mu[j]}, var: {self.var[j]}')
+                    self.distribution[i][j] = multivariate_normal.pdf(data[i], mean=self.mu[j],cov=self.var[j])
+
+    def predict(self, data):
+        result_x = [[] for _ in range(self.n_clusters)]
+        result_y = [[] for _ in range(self.n_clusters)]
         for i in range(data.shape[0]):
-            result.append(np.argmin(distance[i,:]))
-        return result
+            result_x[np.argmax(self.distribution[i,:])].append(data[i][0])
+            result_y[np.argmax(self.distribution[i,:])].append(data[i][1])
+            # result[np.argmax(self.distribution[i,:])].append(data[i])
+
+        return result_x,result_y
 
 # 生成仿真数据
 def generate_X(true_Mu, true_Var):
+    '''
+    generate data points, shape(number, 2)
+    '''
     # 第一簇的数据
     num1, mu1, var1 = 400, true_Mu[0], true_Var[0]
     X1 = np.random.multivariate_normal(mu1, np.diag(var1), num1)
@@ -92,12 +128,13 @@ def generate_X(true_Mu, true_Var):
     # 合并在一起
     X = np.vstack((X1, X2, X3))
     # 显示数据
-    plt.figure(figsize=(10, 8))
-    plt.axis([-10, 15, -5, 15])
-    plt.scatter(X1[:, 0], X1[:, 1], s=5)
-    plt.scatter(X2[:, 0], X2[:, 1], s=5)
-    plt.scatter(X3[:, 0], X3[:, 1], s=5)
-    plt.show()
+    # plt.figure(figsize=(10, 8))
+    # plt.axis([-10, 15, -5, 15])
+    # plt.scatter(X1[:, 0], X1[:, 1], s=5)
+    # plt.scatter(X2[:, 0], X2[:, 1], s=5)
+    # plt.scatter(X3[:, 0], X3[:, 1], s=5)
+    # plt.show()
+    # import ipdb;ipdb.set_trace()
     return X
 
 if __name__ == '__main__':
@@ -108,8 +145,21 @@ if __name__ == '__main__':
 
     gmm = GMM(n_clusters=3)
     gmm.fit(X)
-    cat = gmm.predict(X)
-    print(cat)
+    res_x,res_y = gmm.predict(X)
+
+    plt.figure(figsize=(10, 8))
+    plt.axis([-10, 15, -5, 15])
+    # import ipdb;ipdb.set_trace()
+
+    plt.scatter(res_x[0], res_y[0], s=5)
+    plt.scatter(res_x[1], res_y[1], s=5)
+    plt.scatter(res_x[2], res_y[2], s=5)
+    # plt.scatter(X2[:, 0], X2[:, 1], s=5)
+    # plt.scatter(X3[:, 0], X3[:, 1], s=5)
+    plt.show()
+    # import ipdb;ipdb.set_trace()
+    print(res_x,res_y)
+    # print(cat)
     # 初始化
 
     
